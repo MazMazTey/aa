daa1d5b95a328b4e6525ef2a47e677a940cbe443
Mazdak <mazdak.tey@gmail.com>
2023-05-19 13:17:34 +0330

    added settings access and exit to mainMenu

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import view.ProfileMenu;
+import view.RegisterMenu;
+import view.Settings;
+
+import static javafx.application.Platform.exit;
+
 public class MainMenuController {
+    public void startNewGame() {
+
+    }
+
+    public void continueGame() {
+
+    }
+
+    public void enterProfileMenu() throws Exception {
+        new ProfileMenu().start(RegisterMenu.stage);
+    }
+
+    public void leaderBoard() {
+
+    }
+
+    public void settings() throws Exception {
+        new Settings().start(RegisterMenu.stage);
+    }
+
+    public void Exit() {
+        // TODO save users to gson file
+        exit();
+    }
 }
--- src/main/java/view/MainMenuController.java
+++ src/main/java/view/MainMenuController.java
@@ -1,30 +1,45 @@
 package view;
 
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
+import model.AA;
 
 public class MainMenuController {
-    public void startNewGame(MouseEvent mouseEvent) {
+    private final controller.MainMenuController controller;
 
+    public MainMenuController() {
+        controller = new controller.MainMenuController();
+    }
+
+    public void startNewGame(MouseEvent mouseEvent) {
+        controller.startNewGame();
     }
 
     public void continueGame(MouseEvent mouseEvent) {
-
+        controller.continueGame();
     }
 
-    public void enterProfileMenu(MouseEvent mouseEvent) {
-
+    public void enterProfileMenu(MouseEvent mouseEvent) throws Exception {
+        if (AA.getLoggedInUser().getUsername().equals("guest")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Cannot enter profile menu");
+            alert.setContentText("Guests cannot enter profile menu");
+            alert.showAndWait();
+            return;
+        }
+        controller.enterProfileMenu();
     }
 
     public void leaderBoard(MouseEvent mouseEvent) {
-
+        controller.leaderBoard();
     }
 
     public void settings(MouseEvent mouseEvent) throws Exception {
-        new Settings().start(RegisterMenu.stage);
+        controller.settings();
     }
 
-    public void exit(MouseEvent mouseEvent) {
-        //TODO save users to gson file
-
+    public void Exit(MouseEvent mouseEvent) {
+        controller.Exit();
     }
 }
