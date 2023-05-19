3a1ac141a3506d651a221fe1993c43099b5a1a74
Mazdak <mazdak.tey@gmail.com>
2023-05-19 13:15:24 +0330

    updated user fields and methods

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,17 +1,30 @@
 package model;
 
+import javafx.scene.image.ImageView;
+
 public class User {
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
     private String difficulty;
+    private int highScore;
+    private ImageView avatar;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        highScore = 0;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getPassword() {
@@ -24,5 +37,21 @@ public class User {
 
     public void setDifficulty(String difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void addToHighScore(int amount) {
+        highScore += amount;
+    }
+
+    public ImageView getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(ImageView avatar) {
+        this.avatar = avatar;
     }
 }
